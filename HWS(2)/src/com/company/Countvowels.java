9819788a5aff6c777eeb0034bd78a75a8979127d
fatemesoleymanian <71789169+fatemesoleymanian@users.noreply.github.com>
2020-09-25 15:04:47 -0700

    first commit

---
+++ HWS(2)/src/com/company/Countvowels.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Countvowels {
+    public int count;
+    public void countVowels(String string){
+        for(int i=0 ;i< string.length() ;i++){
+
+        }
+
+    }
+}
--- HWS(2)/src/com/company/Main.java
+++ HWS(2)/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here+
+        Scanner scanner=new Scanner(System.in);
+        String string = null;
+        System.out.println("plz enter a string");
+        Countvowels count=new Countvowels();
+        string=scanner.next();
+        count.countVowels(string);
     }
 }
