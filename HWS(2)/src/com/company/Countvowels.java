909aef8b27abf6bca63953f8fffb79a17316489f
fatemesoleymanian <71789169+fatemesoleymanian@users.noreply.github.com>
2020-09-25 15:18:08 -0700

    first commit

--- HWS(2)/src/com/company/Countvowels.java
+++ HWS(2)/src/com/company/Countvowels.java
@@ -1,11 +1,14 @@
 package com.company;
 
 public class Countvowels {
-    public int count;
+    public int count=0;
     public void countVowels(String string){
         for(int i=0 ;i< string.length() ;i++){
-
+            if (('e' == string.charAt(i)) || ('a' == string.charAt(i)) || ('i' == string.charAt(i)) || ('u' == string.charAt(i)) || ('o' == string.charAt(i))) {
+                count++;
+            }
         }
+        System.out.println(count+"vowel sounds");
 
     }
 }
